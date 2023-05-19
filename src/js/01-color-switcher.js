7b4d10d402f179147b7d9baa089e97ae217af124
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-05-19 11:37:18 +0200

    completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+stopBtn.setAttribute('disabled', '');
+
+startBtn.addEventListener('click', () => {
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+  setRandomBgColor();
+  timerId = setInterval(() => {
+    setRandomBgColor();
+  }, 1000);
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled');
+  //  document.body.removeAttribute('style');
+});
+
+function setRandomBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
